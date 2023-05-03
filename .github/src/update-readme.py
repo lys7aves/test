0f59f5fdec26e261e2e16ff653cc9cda1dc7bee6
Yuseop Lee <90027671+lys7aves@users.noreply.github.com>
2023-05-03 15:57:55 +0900

    Update update-readme.py

--- .github/src/update-readme.py
+++ .github/src/update-readme.py
@@ -1,5 +1,16 @@
 import os
 
-os.('echo "File list" > files.txt')
-os.('find . -type f -printf "- %p %TY-%Tm-%Td %TH:%TM\n" | sed \'s/\.\///\' >> files.txt')
-os.('echo "::set-output name=filelist::$(cat files.txt)"')
+dir_path = "/"
+file_paths = []
+
+# 디렉토리와 그 하위 디렉토리에서 파일 경로를 추출하여 file_paths 리스트에 추가
+for root, directories, files in os.walk(dir_path):
+    for filename in files:
+        file_path = os.path.join(root, filename)
+        file_paths.append(file_path)
+
+# README.md 파일을 열어 파일 경로를 추가
+with open("README.md", "a") as f:
+    f.write("## 파일 목록\n\n")
+    for file_path in file_paths:
+        f.write("- {}\n".format(file_path))
