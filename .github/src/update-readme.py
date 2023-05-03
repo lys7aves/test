b5e55265363a8edc962cf33dac82dc873cd0a947
Yuseop Lee <90027671+lys7aves@users.noreply.github.com>
2023-05-03 16:42:53 +0900

    Update update-readme.py

--- .github/src/update-readme.py
+++ .github/src/update-readme.py
@@ -3,20 +3,20 @@ from datetime import datetime
 
 dir_path = "."
 
-def find_target(path):
+def find_target(path, level):
     for item in os.listdir(path):
         item_path = os.path.join(path, item)
         
         if True: # 특정 조건:
             # 수정 날짜 가져오기
             mtime = datetime.fromtimestamp(os.stat(item_path).st_mtime)
-            target_list.append([item, item_path, mtime])
+            target_list.append([level, item, item_path, mtime])
             
         if os.path.isdir(item_path):
-            find_target(item_path)
+            find_target(item_path, level+1)
 
 target_list = []
-find_target(dir_path)
+find_target(dir_path, 0)
 
 # README.md 파일을 열어 파일 경로를 추가
 with open("README.md", "w") as f:
@@ -25,4 +25,6 @@ with open("README.md", "w") as f:
 
     f.write("## List of notes\n")
     for target in target_list:
-        f.write("- [{}]({}) - {}\n".format(target[0], target[1], target[2]))
+        for level in len(target[0]):
+            f.write("  ")
+        f.write("- [{}]({}) - {}\n".format(target[1], target[2], target[3]))
