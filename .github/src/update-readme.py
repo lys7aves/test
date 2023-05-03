c7d49783c9c59d9c38053b3c5c5d9db2ab47cc8e
Yuseop Lee <90027671+lys7aves@users.noreply.github.com>
2023-05-03 17:05:17 +0900

    Update update-readme.py

--- .github/src/update-readme.py
+++ .github/src/update-readme.py
@@ -1,19 +1,46 @@
 import os
 from datetime import datetime
+import re
 
+# 탐색할 root 경로
 dir_path = "."
 
+# ignore 파일을 읽어서 패턴 목록을 리스트로 저장
+with open('./.github/config/.ignore', 'r') as f:
+    patterns = f.readlines()
+patterns = [p.strip() for p in patterns]
+
+
+# ignore 패턴과 일치하는지 확인하는 함수
+def check_ignore_pattern(item_path):
+    for pattern in patterns:
+        if re.match(pattern, filepath):
+            return True
+    return False
+
+
 def find_target(path, level):
+    cnt = 0
+    
+    # 하위 디렉토리 순환
     for item in os.listdir(path):
         item_path = os.path.join(path, item)
         
-        if True: # 특정 조건:
-            # 수정 날짜 가져오기
-            mtime = datetime.fromtimestamp(os.stat(item_path).st_mtime)
-            target_list.append([level, item, item_path, mtime])
+        if check_ignore_pattern(item_path): # ignore 조건을 만족하면 무시
+            continue
             
+        # 파일(혹은 디렉토리) 리스트에 추가하기
+        mtime = datetime.fromtimestamp(os.stat(item_path).st_mtime)  # 수정 날짜 가져오기
+        target_list.append([level, item, item_path, mtime])
+        cnt += 1
+        
+        # 디렉토리면 하위 디렉토리 탐색
         if os.path.isdir(item_path):
-            find_target(item_path, level+1)
+            res = find_target(item_path, level+1)
+            if res == 0:  # 만약 하위 디렉토리에 아무것도 없으면 현재 디렉토리도 삭제
+                target_list.pop()
+                cnt -= 1
+                
 
 target_list = []
 find_target(dir_path, 0)
