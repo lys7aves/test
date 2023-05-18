e36bb93357c09a76330587dd5d54906c32820902
Yuseop Lee <90027671+lys7aves@users.noreply.github.com>
2023-05-18 17:07:18 +0900

    Update update_timestamps.sh

--- update_timestamps.sh
+++ update_timestamps.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 echo "# Commit Timestamps" > README.md
-git log --format="%ai %h" --name-only | awk '
-  {
-    if ($1 ~ /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/) {
-      date = $0;
-    } else if ($1 ~ /^[0-9]{2}:[0-9]{2}:[0-9]{2}$/) {
-      time = $0;
-    } else if ($1 != "") {
-      printf("- %s %s %s\n", date, time, $0);
+
+git log --name-status --pretty=format:"%ai %h" | awk '
+  /^commit/ {
+    if (commit != "") {
+      printf("- %s %s %s\n", date, time, file);
+      file = "";
     }
-  }' | sort | uniq >> README.md
+    commit = $2;
+  }
+  /^Date:/ {
+    date = $2;
+    time = $3;
+  }
+  /^[A,C,D,M,R,T,U]/ {
+    file = $2;
+  }
+  END {
+    if (commit != "") {
+      printf("- %s %s %s\n", date, time, file);
+    }
+  }
+' | sort | uniq >> README.md
